70d3f6ef5e152956257cc70dfad5e425377f0f9f
Богданова Алена Михайловна <albogdanova@sberhealth.ru>
2022-05-29 23:38:45 +0300

    added owner

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -2,8 +2,10 @@ package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -14,11 +16,14 @@ public class TestBase {
 
     @BeforeAll
     static void setUp() {
+        CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         Configuration.baseUrl = System.getProperty("pageUrl", "https://demoqa.com");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.remote = "https://user1:1234" + System.getProperty("selenoidUrl", "@selenoid.autotests.cloud/wd/hub");
+        Configuration.remote = String.format("https://%s:%s%s",
+                config.login(), config.password(), System.getProperty("selenoidUrl", "@selenoid.autotests.cloud/wd/hub"));
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
